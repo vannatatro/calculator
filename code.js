66bb7481ad704d18d507fe3451daddd5643acfeb
Savanna <149965311+vannatatro@users.noreply.github.com>
2024-01-31 16:13:01 -0500

    Add clear buttons

--- code.js
+++ code.js
@@ -1,75 +1,135 @@
 // Make functions
-let num1 = 6
-let num2 = 3
+// let num1 = ""
+// let num2 = ""
 
-var input = "error"
-let expressionInput = document.querySelector("#expression")
-expressionInput.textContent = input
+let num = ""
+let solve = ""
+let expressionInput = document.querySelector("#expression");
 
-const add = num1 + num2
-const subtract = num1 - num2
-const divide = num1 / num2
-const multiply = num1 * num2
+function errorCondition() {
+    if (num.length >= 15) {
+        expressionInput.textContent = 'error'
+    }
 
-console.log(add)
-console.log(subtract)
-console.log(divide)
-console.log(multiply)
+}
+
+// const add = num1 + num2
+// const subtract = num1 - num2
+// const divide = num1 / num2
+// const multiply = num1 * num2
+
+// console.log(add)
+// console.log(subtract)
+// console.log(divide)
+// console.log(multiply)
 
 // Make buttons work
 
 let one = document.querySelector('#oneBtn')
 one.addEventListener ('click', () => {
-    console.log('1')
+    solve += "1";
+    num += "1";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let two = document.querySelector('#twoBtn')
 two.addEventListener ('click', () => {
-    console.log('2')
+    solve += "2";
+    num += "2";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let three = document.querySelector('#threeBtn')
 three.addEventListener ('click', () => {
-    console.log('3')
+    solve += "3";
+    num += "3";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let four = document.querySelector('#fourBtn')
 four.addEventListener ('click', () => {
-    console.log('4')
+    solve += "4";
+    num += "4";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let five = document.querySelector('#fiveBtn')
 five.addEventListener ('click', () => {
-    console.log('5')
+    solve += "5";
+    num += "5";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let six = document.querySelector('#sixBtn')
 six.addEventListener ('click', () => {
-    console.log('6')
+    solve += "6";
+    num += "6";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let seven = document.querySelector('#sevenBtn')
 seven.addEventListener ('click', () => {
-    console.log('7')
+    solve += "7";
+    num += "7";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let eight = document.querySelector('#eightBtn')
 eight.addEventListener ('click', () => {
-    console.log('8')
+    solve += "8";
+    num += "8";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let nine = document.querySelector('#nineBtn')
 nine.addEventListener ('click', () => {
-    console.log('9')
+    solve += "9";
+    num += "9";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 let zero = document.querySelector('#zeroBtn')
 zero.addEventListener ('click', () => {
-    console.log('0')
+    solve += "0";
+    num += "0";
+    expressionInput.textContent = num;
+    errorCondition();
 })
 
 
 let decimal = document.querySelector('#decimalBtn')
 decimal.addEventListener ('click', () => {
-    console.log('.')
+    solve += ".";
+    num += ".";
+    expressionInput.textContent = num;
+    errorCondition();
 })
+
+// Make operator buttons
+let subtract = document.querySelector('#subBtn')
+subtract.addEventListener ('click', () => {
+    solve += " - ";
+    num = "";
+    expressionInput.textContent = num;
+    console.log(solve)
+    console.log(num)
+}  )
+
+let multiply = document.querySelector('#multiBtn')
+multiply.addEventListener ('click', () => {
+    solve += " × "
+    expressionInput.textContent = "";
+})
+
+
+
+
